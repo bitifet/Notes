4744bec12599c50c4fe7cae24cd691af0e052290
Joan Miquel Torres <joanmiquel@mallorcaweb.net>
2014-10-23 16:24:50 +0200

    Notes i androidenv.sh del curs de Cordova/Phonegap.

---
+++ Cordova/res/androidenv.sh
@@ -0,0 +1,7 @@
+export OPT_DIR="/home/joanmi/opt"
+export SDK_DIR="${OPT_DIR}/android/adt-bundle-linux-x86_64-20140702/sdk"
+
+export PATH="${PATH}:${SDK_DIR}/platform-tools";
+export PATH="${PATH}:${SDK_DIR}/tools";
+export PATH="${PATH}:${SDK_DIR}/build-tools";
+
